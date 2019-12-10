1067c5f65354a3dafe09f0e93610dfe976e65558
hirokik0811 <32285179+hirokik0811@users.noreply.github.com>
2019-12-10 00:36:12 -0500

    You may save the matrix into an external file

--- TrianglePlaquette/TrianglePlaquette/TrianglePlaquette.c
+++ TrianglePlaquette/TrianglePlaquette/TrianglePlaquette.c
@@ -1,17 +1,21 @@
 // TrianglePlaquette.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#include <assert.h>
 #include <stdio.h>
 #include <math.h>
 #include <mkl.h>
+#include <stdbool.h>
 #include "triangle_plaquette_hamiltonian.h"
 //#include "square_tessellation.h"
+
 //#define N_LAYERS 2
 //#define G 0.1
 //#define ALPHA 1.0
 //#define EMIN 0.0 // the minimum of the search range of the feast algorithm
 //#define EMAX 1000.0 // the maxmum 
 
+
+// the arguments must be: N_LAYERS, G, ALPHA, EMIN, EMAX, -o outputfilename or/and -i inputfilename
 int main(int argc, char* argv[])
 {
 #define CALL_AND_CHECK_STATUS(function, error_message) do { \
@@ -23,16 +27,39 @@ int main(int argc, char* argv[])
 		  }                                                 \
 } while(0)
 
-	if (argc != 6) {
+	int i = 0, ii = 0, j = 0;
+
+	if (argc != 6 && argc != 8 && argc != 10) {
 		printf("invalid number of arguments. Aborting...");
 		return 1;
 	}
 
-	int nLayers = atoi(argv[1]);
-	double g = atof(argv[2]), alpha = atof(argv[3]), Emin= atof(argv[4]), Emax = atof(argv[5]);
-	
+	const int nLayers = atoi(argv[1]);
+	const double g = atof(argv[2]), alpha = atof(argv[3]), Emin= atof(argv[4]), Emax = atof(argv[5]);
 
-	int i = 0, ii = 0, j = 0;
+	// if input file or output file storing the Hamiltonian matrix is specified, use them. 
+	FILE *omatfile = NULL;
+	FILE *imatfile = NULL;
+	bool save_mat = false, load_mat = false;
+	if (argc >= 8) {
+		for (i = 6; i < argc-1; i+=2) {
+			if (strcmp(argv[i], "-o") == 0) {
+				omatfile = fopen(argv[i + 1], "w");
+				if (omatfile == NULL) 
+					printf("Cannot open the matrix output file. Aborting...");
+				else
+				    save_mat = true;
+			}
+			else if (strcmp(argv[i], "-i") == 0) {
+				imatfile = fopen(argv[i + 1], "r");
+				if (imatfile == NULL)
+					printf("Cannot open the matrix input file. Aborting...");
+				else
+					load_mat = true;
+			}
+			else { printf("invalid flag %s. Aborting...", argv[i]); }
+		}
+	}
 
 	sparse_status_t status = SPARSE_STATUS_SUCCESS; // stores the status of MKL function evaluations.
 	sparse_matrix_t H = NULL;
@@ -63,53 +90,86 @@ int main(int argc, char* argv[])
 	//
 
 
-	// Compute the Hamiltonian matrix of the triangle model and store it to H
 	
 	
-	// test Pauli operator
-	
-	double* coefList = malloc(20 * sizeof(double));
-	int** listOfPauliList = malloc(20*sizeof(int*));
-	for (i = 0; i < 20; ++i) {
-		coefList[i] = 1.0;
-		listOfPauliList[i] = calloc(20, sizeof(int));
+	if (load_mat) { // Load the Hamiltonian from the file
+		char line[128];
+		int line_cnt = 0; // count the lines
+		MKL_INT row = 0, col = 0;
+		double real = 0.0, imag = 0.0;
+		MKL_INT prev_row = 0;
+		while ((fgets(line, sizeof line, imatfile)) != NULL) {
+			printf("Retrieved line %s\n", line);
+			if (line_cnt == 0) {
+				valuesP = (MKL_Complex16 *)mkl_malloc(atoi(line) * sizeof(MKL_Complex16), 64);
+				columns_P = (MKL_INT*)mkl_malloc(atoi(line) * sizeof(MKL_INT), 64);
+			} else if (line_cnt == 1) {
+				pointerB_P = (MKL_Complex16*)mkl_malloc(atoi(line) * sizeof(MKL_Complex16), 64);
+				pointerE_P = (MKL_Complex16*)mkl_malloc(atoi(line) * sizeof(MKL_Complex16), 64);
+				n_rowsP = atoi(line);
+				n_colsP = atoi(line);
+			}
+			else {
+				sscanf(line, " { %d , %d } -> %lf + %lf I", &row, &col, &real, &imag);
+				MKL_Complex16 val = { real, imag };
+				valuesP[line_cnt - 2] = val;
+				if (prev_row != row) {
+					prev_row = row;
+					pointerB_P[row-1] = line_cnt-1;
+					if (row > 1) pointerE_P[row - 2] = line_cnt - 1;
+				}
+				columns_P[line_cnt - 2] = col;
+			}
+			line_cnt++;
+		}
+		pointerE_P[n_rowsP - 1] = line_cnt - 1;
+		fclose(imatfile);
 	}
-	
-	//CALL_AND_CHECK_STATUS(pauli_hamiltonian_matrix(&H, 20, 20, listOfPauliList, coefList), "Error during computing a triangle plaquette matrix");
-	
-	CALL_AND_CHECK_STATUS(triangle_plaquette_hamiltonian_matrix(&H, nLayers, g, alpha), "Error during computing a triangle plaquette matrix");
-	//CALL_AND_CHECK_STATUS(square_tessellation_hamiltonian_matrix(&H, nLayers, g, alpha), "Error during computing 8 triangles model Hamiltonian matrix");
-	// Compute the block Hamiltonian with zero flux
-	CALL_AND_CHECK_STATUS(zero_gauge_block(&zeroH, H, nLayers),
-		"Error during computing a block Hamiltonian corresponding to zero flux\n");
+	else {// Compute the Hamiltonian matrix of the triangle model and store it to H
+		// test Pauli operator
+		/*
+		double* coefList = malloc(20 * sizeof(double));
+		int** listOfPauliList = malloc(20*sizeof(int*));
+		for (i = 0; i < 20; ++i) {
+			coefList[i] = 1.0;
+			listOfPauliList[i] = calloc(20, sizeof(int));
+		}
+		*/
+		//CALL_AND_CHECK_STATUS(pauli_hamiltonian_matrix(&H, 20, 20, listOfPauliList, coefList), "Error during computing a triangle plaquette matrix");
 
-	// Check the data
-	CALL_AND_CHECK_STATUS(mkl_sparse_z_export_csr(zeroH, &indexing, &n_rowsP, &n_colsP, &pointerB_P, &pointerE_P, &columns_P, &valuesP),
-		"Error after MKL_SPARSE_Z_EXPORT_CSR  H\n");
+		CALL_AND_CHECK_STATUS(triangle_plaquette_hamiltonian_matrix(&H, nLayers, g, alpha), "Error during computing a triangle plaquette matrix");
+		//CALL_AND_CHECK_STATUS(square_tessellation_hamiltonian_matrix(&H, nLayers, g, alpha), "Error during computing 8 triangles model Hamiltonian matrix");
+		// Compute the block Hamiltonian with zero flux
+		CALL_AND_CHECK_STATUS(zero_gauge_block(&zeroH, H, nLayers),
+			"Error during computing a block Hamiltonian corresponding to zero flux\n");
 
-	// Convert zero-based indexing to one-based indexing
-	for (i = 0; i < n_rowsP; ++i) {
-		pointerB_P[i] ++;
+		// Check the data
+		CALL_AND_CHECK_STATUS(mkl_sparse_z_export_csr(zeroH, &indexing, &n_rowsP, &n_colsP, &pointerB_P, &pointerE_P, &columns_P, &valuesP),
+			"Error after MKL_SPARSE_Z_EXPORT_CSR  H\n");
+
+		// Convert zero-based indexing to one-based indexing
+		for (i = 0; i < n_rowsP; ++i) {
+			pointerB_P[i] ++;
+		}
+		pointerE_P[n_rowsP - 1]++;
+		for (i = 0; i < pointerE_P[n_rowsP - 1] - 1; ++i) {
+			columns_P[i]++;
+		}
 	}
-	pointerE_P[n_rowsP - 1]++;
-
-	printf("pointerE_P[n_rowsP - 1] - 1 %d\n", (pointerE_P[n_rowsP - 1] - 1));
-	for (i = 0; i < pointerE_P[n_rowsP - 1] - 1; ++i) {
-		columns_P[i]++;
-	}
-
-	printf("Hamiltonian: \n");
-	printf("Shape: %d x %d \n", n_rowsP, n_colsP);
+	printf("%d\n", (pointerE_P[n_rowsP - 1] - 1)); // number of non-zero elements
+	if (save_mat) fprintf(omatfile, "%d\n", (pointerE_P[n_rowsP - 1] - 1));
+	printf("%d\n", n_rowsP); // number of rows
+	if (save_mat) fprintf(omatfile,"%d\n", n_rowsP);
 	for (i = 0; i < n_rowsP; i++)
 	{
 		for (j = pointerB_P[i]; j < pointerE_P[i]; j++)
 		{
-			printf(" {%d, %d} -> %5.3f + %5.3f I,", i+1, columns_P[ii], valuesP[ii].real, valuesP[ii].imag); fflush(0);
+			printf(" { %d , %d } -> %5.3f + %5.3f I,\n", i + 1, columns_P[ii], valuesP[ii].real, valuesP[ii].imag); fflush(0);
+			if (save_mat) fprintf(omatfile, " { %d , %d } -> %5.3f + %5.3f I,\n", i + 1, columns_P[ii], valuesP[ii].real, valuesP[ii].imag);
 			ii++;
 		}
-		printf("\n");
 	}
-
+	fclose(omatfile);
 
 	// Solve eigenvalue problem
 	printf("Computing eigenvalues...\n");
@@ -134,7 +194,7 @@ int main(int argc, char* argv[])
 		&UPLO,   /* IN: UPLO = 'U', stores the upper triangle part of the matrix */
 		&n_rowsP,      /* IN: Size of the problem */
 		valuesP,     /* IN: CSR matrix A, values of non-zero elements */
-		pointerB_P,    /* IN: CSR matrix A, index of the first non-zero element in row */
+		rows_indx,    /* IN: CSR matrix A, index of the first non-zero element in row */
 		columns_P,    /* IN: CSR matrix A, columns indices for each non-zero element */
 		fpm,     /* IN/OUT: Array is used to pass parameters to Intel(R) MKL Extended Eigensolvers */
 		&epsout, /* OUT: Relative error of on the trace */
@@ -195,15 +255,17 @@ memory_free:
 	free(E);
 	free(X);
 	free(res);
-	status = mkl_sparse_destroy(H);
-	if (status != SPARSE_STATUS_SUCCESS)
-	{
-		printf(" Error after MKL_SPARSE_DESTROY(H) \n"); fflush(0);
-	}
-	status = mkl_sparse_destroy(zeroH);
-	if (status != SPARSE_STATUS_SUCCESS)
-	{
-		printf(" Error after MKL_SPARSE_DESTROY(zeroH) \n"); fflush(0);
+	if (!load_mat) {
+		status = mkl_sparse_destroy(H);
+		if (status != SPARSE_STATUS_SUCCESS)
+		{
+			printf(" Error after MKL_SPARSE_DESTROY(H) \n"); fflush(0);
+		}
+		status = mkl_sparse_destroy(zeroH);
+		if (status != SPARSE_STATUS_SUCCESS)
+		{
+			printf(" Error after MKL_SPARSE_DESTROY(zeroH) \n"); fflush(0);
+		}
 	}
 	mkl_free_buffers();
 
